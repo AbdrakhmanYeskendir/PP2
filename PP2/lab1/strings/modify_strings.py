b99851fb49014f3f01c319f6094cf90516a15ffa
AbdrakhmanYeskendir <yeskendir.abdrakhman24@gmail.com>
2025-02-01 15:31:34 +0500

    Add files via upload

---
+++ PP2/lab1/strings/modify_strings.py
@@ -0,0 +1,20 @@
+#Upper Case 
+a = "Hello, World!"
+print(a.upper())
+
+#Lower Case
+a = "Hello, World!"
+print(a.lower())
+
+#Remove Whitespace
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+#Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Split String
+#The split() method splits the string into substrings if it finds instances of the separator:
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
